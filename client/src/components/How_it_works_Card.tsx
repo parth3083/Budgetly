d0b23d3f75d8f102d24b1fca7a585c0510547091
parth3083 <parthbrajput30@gmail.com>
2024-08-04 00:20:53 +0530

    How it works updated

--- client/src/components/How_it_works_Card.tsx
+++ client/src/components/How_it_works_Card.tsx
@@ -1,7 +1,6 @@
-"use client";
-import { cn } from "../lib/utils";
+import { GlareCard } from "../components/ui/glare-card";
 
-export default function CardDemo() {
+export default function GlareCardDemo() {
   const data = [
     {
       title: "Sign Up",
@@ -19,31 +18,26 @@ export default function CardDemo() {
         "Receive personalized insights and recommendations based on your spending habits. Understand where your money goes!",
     },
     {
-      title: "Optimize Your Budget",
+      title: "Optimize Budget",
       description:
         "Set and manage budgets to reach your financial goals. Let our AI-powered tools guide you toward smarter spending.",
     },
   ];
   return (
-    <div className="w-[100%]  flex flex-col md:flex-row gap-5 md:gap-0 items-center justify-center">
-      {data.map((item, ind) => (
-        <div
+    <section className="w-full flex items-center justify-between px-5 0 h-fit">
+      {data.map((items, ind) => (
+        <GlareCard
           key={ind}
-          className={cn(
-            "w-[22%] cursor-pointer overflow-hidden relative card h-96 rounded-md shadow-xl  max-w-sm mx-auto backgroundImage flex flex-col justify-between p-4",
-            "bg-[url(https://4kwallpapers.com/images/walls/thumbs_3t/8324.png)] bg-cover"
-          )}
+          className="flex flex-col items-center px-2 justify-center"
         >
-          <div className="text content flex flex-col justify-end h-full">
-            <h1 className="font-bold text-xl md:text-2xl text-gray-50 relative z-10">
-              Step {ind + 1} : {item.title}
-            </h1>
-            <p className="font-normal text-sm text-gray-50 relative z-10 my-4">
-              {item.description}
-            </p>
-          </div>
-        </div>
+          <h1 className="font-bold text-xl md:text-3xl text-gray-50 relative z-10">
+            {items.title}
+          </h1>
+          <p className="font-normal text-center text-sm text-gray-50 relative z-10 my-4">
+            {items.description}
+          </p>
+        </GlareCard>
       ))}
-    </div>
+    </section>
   );
 }
