58dc14d7ce9c60aaa8d831f50dc7fc89040c21f4
parth3083 <parthbrajput30@gmail.com>
2024-08-06 20:06:29 +0530

    Benefits Section completed successfully with responsiveness

--- client/src/components/home page/HeroSection4.tsx
+++ client/src/components/home page/HeroSection4.tsx
@@ -3,8 +3,8 @@ import Marquee from "../Marquee";
 function HeroSection4() {
   return (
     <section className="w-full h-fit flex flex-col items-center  mt-16">
-      <h1 className="text-4xl sm:text-base md:text-xl lg:text:4xl xl:text-6xl text-white text-center font-bold">
-        Benefits Section
+      <h1 className="text-4xl sm:text-base md:text-3xl lg:text:4xl xl:text-6xl text-white text-center font-bold">
+        Benefits 
       </h1>
       <Marquee />
     </section>
--- client/src/components/ui/infinite-moving-cards.tsx
+++ client/src/components/ui/infinite-moving-cards.tsx
@@ -86,7 +86,7 @@ export const InfiniteMovingCards = ({
       >
         {items.map((item, idx) => (
           <li
-            className="w-[350px] max-w-full relative rounded-2xl border border-b-0 flex-shrink-0 border-slate-700 px-8 py-6 md:w-[450px]"
+            className="w-[350px] max-w-full h-[50vw] md:h-[16vw] lg:h-[10vw] relative rounded-2xl border border-b-0 flex-shrink-0 border-slate-700 px-5 py-2 md:w-[450px]"
             style={{
               background:
                 "linear-gradient(180deg, var(--slate-800), var(--slate-900)",
@@ -98,12 +98,12 @@ export const InfiniteMovingCards = ({
                 className="user-select-none -z-1 pointer-events-none absolute -left-0.5 -top-0.5 h-[calc(100%_+_4px)] w-[calc(100%_+_4px)]"
               ></div>
 
-              <div className="relative z-20 mt-2 flex flex-row items-center">
+              <div className="relative z-20   flex flex-row items-center">
                 <span className="flex flex-col gap-1">
                   <span className=" text-2xl font-bold leading-[1.6] text-gray-100 ">
                     {item.title}
                   </span>
-                  <span className=" relative z-20 text-sm leading-[1.6] text-gray-400 font-normal">
+                  <span className=" relative z-20 lg:text-lg text-md leading-[1.6] text-gray-400 font-normal">
                     {item.quote}
                   </span>
                 </span>
